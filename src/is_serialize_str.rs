aa52b675e01dff4464d000585c3f23e5815bf2fb
David Tolnay <dtolnay@gmail.com>
2024-07-28 15:18:23 -0700

    Shorten names of SerializerState variants

--- src/is_serialize_str.rs
+++ src/is_serialize_str.rs
@@ -9,8 +9,8 @@ use serde::ser::{self, Error, Impossible, Serialize, StdError};
 pub fn is_serialize_str<T: ?Sized + Serialize>(value: &T, expected_str: &'static str) -> bool {
     match value.serialize(Serializer { expected_str }) {
         Ok(void) => match void {},
-        Err(SerializerState::GotExpectedStr) => true,
-        Err(SerializerState::GotUnexpected) => false,
+        Err(SerializerState::Ok) => true,
+        Err(SerializerState::Unexpected) => false,
     }
 }
 
@@ -18,13 +18,13 @@ enum Void {}
 
 #[derive(Debug)]
 enum SerializerState {
-    GotExpectedStr,
-    GotUnexpected,
+    Ok,
+    Unexpected,
 }
 
 impl Error for SerializerState {
     fn custom<M: Display>(_message: M) -> Self {
-        SerializerState::GotUnexpected
+        SerializerState::Unexpected
     }
 }
 
@@ -53,79 +53,79 @@ impl ser::Serializer for Serializer {
     type SerializeStructVariant = Impossible<Self::Ok, Self::Error>;
 
     fn serialize_bool(self, _: bool) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_i8(self, _: i8) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_i16(self, _: i16) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_i32(self, _: i32) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_i64(self, _: i64) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_u8(self, _: u8) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_u16(self, _: u16) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_u32(self, _: u32) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_u64(self, _: u64) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_f32(self, _: f32) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_f64(self, _: f64) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_char(self, _: char) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
         if v == self.expected_str {
-            Err(SerializerState::GotExpectedStr)
+            Err(SerializerState::Ok)
         } else {
-            Err(SerializerState::GotUnexpected)
+            Err(SerializerState::Unexpected)
         }
     }
 
     fn serialize_bytes(self, _: &[u8]) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_some<T: ?Sized + Serialize>(self, _: &T) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_unit_struct(self, _name: &'static str) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_unit_variant(
@@ -134,7 +134,7 @@ impl ser::Serializer for Serializer {
         _variant_index: u32,
         _variant: &'static str,
     ) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_newtype_struct<T: ?Sized + Serialize>(
@@ -142,7 +142,7 @@ impl ser::Serializer for Serializer {
         _name: &'static str,
         _value: &T,
     ) -> Result<Self::Ok, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_newtype_variant<T>(
@@ -155,15 +155,15 @@ impl ser::Serializer for Serializer {
     where
         T: ?Sized + Serialize,
     {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_tuple(self, _len: usize) -> Result<Self::SerializeTuple, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_tuple_struct(
@@ -171,7 +171,7 @@ impl ser::Serializer for Serializer {
         _name: &'static str,
         _len: usize,
     ) -> Result<Self::SerializeTupleStruct, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_tuple_variant(
@@ -181,11 +181,11 @@ impl ser::Serializer for Serializer {
         _variant: &'static str,
         _len: usize,
     ) -> Result<Self::SerializeTupleVariant, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_struct(
@@ -193,7 +193,7 @@ impl ser::Serializer for Serializer {
         _name: &'static str,
         _len: usize,
     ) -> Result<Self::SerializeStruct, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 
     fn serialize_struct_variant(
@@ -203,7 +203,7 @@ impl ser::Serializer for Serializer {
         _variant: &'static str,
         _len: usize,
     ) -> Result<Self::SerializeStructVariant, Self::Error> {
-        Err(SerializerState::GotUnexpected)
+        Err(SerializerState::Unexpected)
     }
 }
 
